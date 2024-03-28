ff98c0adb76d0a6624daa02ebc59e0dd0dcdd415
Oleg <oopolunin@gmail.com>
2024-03-28 16:43:10 +0300

    add lesson 7

---
+++ lesson-7.py
@@ -0,0 +1,15 @@
+#Задание 1
+input_string = input("Введите строку без пробелов: ")
+
+if input_string == input_string[::-1]:
+   print("yes")
+else:
+    print("no")
+
+# Задание 2
+input_string = input("Введите строку: ")
+
+output_string = ' '.join(input_string.split())
+
+print("Измененная строка:")
+print(output_string)
